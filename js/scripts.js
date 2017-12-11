9c7de5bc14f05b33e8e7f56214975f15ca398cc2
Aaron Ross and Zach Evans <zach@>
2017-12-11 09:03:28 -0800

    add basic html form to collect user input

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,8 @@ function Place(name, location, landmarks, timeOfYear, notes) {
 
 
 //front end -------------
+$(document).ready(function() {
+  $("#form").submit(function(event) {
+    event.preventDefault();
+  });
+});
